915721b4970ab8b2095305a6411fa5d752070be2
GodHVoid <renatomillanx56@gmail.com>
2025-05-20 04:12:25 +0000

    connection to tor works

---
+++ src/toralizer.h
@@ -0,0 +1,52 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define PROXY "127.0.0.1"
+#define PROXYPORT 9050
+
+typedef unsigned char int8;
+typedef unsigned short int int16;
+typedef unsigned int int32;
+
+/*
+
++----+----+----+----+----+----+----+----+----+----+....+----+
+| VN | CD | DSTPORT |      DSTIP        | USERID       |NULL|
++----+----+----+----+----+----+----+----+----+----+....+----+
+ # of bytes:
+  1    1      2              4           variable        1
+
+ */
+
+struct proxy_request {
+  int8 vn;
+  int8 cd;
+  int16 dstport;
+  int32 dstip;
+  unsigned char userid[8];
+};
+
+typedef struct proxy_request Req;
+
+/*
+
++----+----+----+----+----+----+----+----+
+| VN | CD | DSTPORT |      DSTIP        |
++----+----+----+----+----+----+----+----+
+ # of bytes:
+  1    1      2              4
+*/
+
+struct proxy_response {
+  int8 vn;
+  int8 cd;
+  int16 _;
+  int32 __;
+};
+
+typedef struct proxy_response Res;
